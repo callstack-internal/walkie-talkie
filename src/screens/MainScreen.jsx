528c5287753305fb2f0d8de026967bd2b53d637c
mrzilvis <bartkuszilvinas@gmail.com>
2024-02-19 14:41:26 +0200

    Add users for testing

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,10 +1,15 @@
-import WalkieTalkieButton from "../components/WalkieTalkieButton";
 import styles from "./MainScreen.module.css";
+import WalkieTalkieButton from "../components/WalkieTalkieButton";
+import Username from "../components/Username";
 
 export default function MainScreen() {
   return (
     <div className={styles.main}>
       <WalkieTalkieButton />
+      <Username username="John Doe" isTalking={true} />
+      <Username username="Jane Doe" isTalking={false} />
+      <Username username="John Smith" isTalking={false} />
+      <Username username="Jane Smith" isTalking={false} />
     </div>
   );
 }
